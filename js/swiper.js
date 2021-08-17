4349c4498e485eba5d144f1c0e35cd41cd26d248
Мария <lpvamaria@gmail.com>
2021-08-17 17:59:04 +0300

    Больше резины

--- js/swiper.js
+++ js/swiper.js
@@ -1,3 +1,5 @@
+"use strict";
+
 new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 10,
@@ -5,29 +7,29 @@ new Swiper(".mySwiper", {
         el: '.swiper-scrollbar',
         draggable: true
     },
-        simulateTouch: true,
-        touchRatio: 1,
-        touchAngle: 45,
-        grabCursor: true,
-        hashNavigation: {
-        watchState: true,
+    simulateTouch: true,
+    touchRatio: 1,
+    touchAngle: 45,
+    grabCursor: true,
+    hashNavigation: {
+        watchState: true
     },
     keyboard: {
         enabled: true,
-        onlyInViewport: true,
+        onlyInViewport: true
     },
     pagination: {
         el: ".swiper-pagination",
-        clickable: true,
+        clickable: true
     },
     breakpoints: {
         768: {
-            spaceBetween: 25,
+        spaceBetween: 25
         },
         1200: {
-            spaceBetween: 35,
-        },
+        spaceBetween: 35
+        }
     },
     watchSlidesProgress: true,
-    watchSlidesvisibility: true,
+    watchSlidesvisibility: true
 });
